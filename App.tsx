2f0d3be6f77bfd6d951eb42ecb328861b701e9bf
Alex Sandro <alexsandrojr.303@gmail.com>
2023-09-03 13:15:26 -0300

    feat: :sparkles: loading fonst

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts as merriwatherFont, Merriweather_400Regular, Merriweather_700Bold } from "@expo-google-fonts/merriweather"
+import {useFonts as latoFont, Lato_400Regular, Lato_700Bold} from "@expo-google-fonts/lato"
+import {useFonts as robotoFont, Roboto_400Regular} from "@expo-google-fonts/roboto"
 
 export default function App() {
+  const [merriwatherLoaded, merriwatherfontError] = merriwatherFont({
+    Merriweather_400Regular,
+    Merriweather_700Bold
+  })
+
+  const [latoLoaded, latoFontError] = latoFont({
+    Lato_400Regular,
+    Lato_700Bold
+  })
+
+  const [robotoLoaded, robotoFontError] = robotoFont({
+    Roboto_400Regular
+  })
+
+  const merriwatherNotLoaded = !merriwatherLoaded && !merriwatherfontError;
+  const latoNotLoaded = !latoLoaded && !latoFontError;
+  const robotoNotLoaded = !robotoLoaded && !robotoFontError;
+
+  if (merriwatherNotLoaded || latoNotLoaded || robotoNotLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello word</Text>
       <StatusBar style="auto" />
     </View>
   );
