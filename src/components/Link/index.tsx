a7779e510294ee23aea827e3fb5000b7f51fdec7
Alex Sandro <alexsandrojr.303@gmail.com>
2023-09-03 14:53:12 -0300

    feat: refactor component

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
+import { LinkProps } from './types';
 
-function ForgetPassword() {
+function Link({ text }: LinkProps) {
   return (
     <TouchableOpacity>
-      <Text style={styles.text}>Esqueceu sua senha?</Text>
+      <Text style={styles.text}>{text}</Text>
     </TouchableOpacity>
   );
 }
 
-export { ForgetPassword }
+export { Link }
