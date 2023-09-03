ad028947f0d6c3e22baf8d4fcab5a80a3ef3b89e
Alex Sandro <alexsandrojr.303@gmail.com>
2023-09-03 13:15:42 -0300

    feat: :sparkles: create a conts to use fonts

---
+++ src/constants/fonts/index.tsx
@@ -0,0 +1,15 @@
+const fonts = {
+  merriwather: {
+    regular: "Merriweather_400Regular",
+    bold: "Merriweather_700Bold"
+  },
+  lato: {
+    regular: "Lato_400Regular",
+    bold: "Lato_700Bold"
+  },
+  roboto: {
+    regular: "Roboto_400Regular"
+  }
+}
+
+export { fonts }
